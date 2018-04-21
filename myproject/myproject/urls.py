c5c30b09ab546ebdb02e60c22a9bcb53a61cc9b6
pariaspe <pedroariasperez96@gamil.com>
2018-04-21 22:09:21 +0200

    Logout implemented

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.contrib.auth.views import logout
 
 urlpatterns = [
     # Examples:
@@ -8,6 +8,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^logout', logout),
     url(r'^$', 'cms_post.views.barra'),
     url(r'^(.*)$', 'cms_post.views.other'),
 ]
