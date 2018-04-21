251dfa49ac33e1b2b0ed4dd1dfa65882a3a34ceb
pariaspe <pedroariasperez96@gamil.com>
2018-04-21 22:16:31 +0200

    login implemented

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth.views import logout
+from django.contrib.auth.views import login
 
 urlpatterns = [
     # Examples:
@@ -9,6 +10,7 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^logout', logout),
+    url(r'^login', login),
     url(r'^$', 'cms_post.views.barra'),
     url(r'^(.*)$', 'cms_post.views.other'),
 ]
