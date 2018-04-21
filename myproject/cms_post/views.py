46697d629fbeb3e26a6ffb9f57f8b33e921b208e
pariaspe <pedroariasperez96@gamil.com>
2018-04-21 21:43:00 +0200

    Barra's basic view implemented

--- myproject/cms_post/views.py
+++ myproject/cms_post/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Pages
+
 # Create your views here.
 
 def barra(request):
-    return HttpResponse('Hola')
+    pages = Pages.objects.all()
+    lista = 'Paginas guardadas:<ul>'
+    for page in pages:
+        lista += '<li>' + page.name + '</li>'
+    lista += '</lu>'
+
+    answer = '<h1>Sistema de gestión de contenido</h1>'
+    answer += lista
+    return HttpResponse(answer)
