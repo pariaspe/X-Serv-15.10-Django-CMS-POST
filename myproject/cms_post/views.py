abfe8c52a36090aafbdaf0b0293329a23106a8c7
pariaspe <pedroariasperez96@gamil.com>
2018-04-22 11:47:40 +0200

    Views improved

--- myproject/cms_post/views.py
+++ myproject/cms_post/views.py
@@ -5,22 +5,32 @@ from django.http import HttpResponseNotFound
 from .models import Pages
 
 # Create your views here.
+def get_log(is_logged_in, username):
+    if is_logged_in:
+        logged = '<p>Logged in as ' + username + '.</br>'
+        logged += '<a href=logout>Logout</a></p>'
+    else:
+        logged = '<p>Not logged in. <a href=login>Login</a></p>'
+    return logged
 
 def barra(request):
     pages = Pages.objects.all()
     lista = 'Paginas guardadas:<ul>'
     for page in pages:
-        lista += '<li>' + page.name + '</li>'
+        lista += '<li><a href="' + page.name + '">' + page.name + '</a></li>'
     lista += '</lu>'
-
     answer = '<h1>Sistema de gestión de contenido</h1>'
     answer += lista
+    answer += get_log(request.user.is_authenticated(), request.user.username)
     return HttpResponse(answer)
 
 def other(request, recurso):
     try:
         page = Pages.objects.get(name=recurso)
-        return HttpResponse(page.page)
+        answer = page.page + '<p><a href="/">Inicio</a></p>'
+        answer += get_log(request.user.is_authenticated(), request.user.username)
+        return HttpResponse(answer)
     except Pages.DoesNotExist:
         answer = 'La pagina ' + recurso + ' no está guardada.'
+        answer += '<p><a href="/">Inicio</a></p>'
         return HttpResponseNotFound(answer)
