a8c251fcaf2932337b3fd965bc7cf6f89abe5492
pariaspe <pedroariasperez96@gamil.com>
2018-04-22 14:20:27 +0200

    Form add to barra and barra_annotated views

--- myproject/cms_post/views.py
+++ myproject/cms_post/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseNotFound
+from django.db import IntegrityError
+from django.views.decorators.csrf import csrf_exempt
 from django.template.loader import get_template
 from django.template import Context
 
 from .models import Pages
 
 # Create your views here.
+FORM = """
+    <form action="" method="POST">
+        <label for="name">Pagina:</label><br>
+        <input type="text" name="name" value=""/><br>
+        <label for="name">Contenido:</label><br>
+        <input type="text" name="page" value=""/><br>
+        <input type="submit" value="Send">
+    </form>
+"""
+
 def get_log(is_logged_in, username):
     if is_logged_in:
         logged = '<p>Logged in as ' + username + '.</br>'
@@ -15,25 +27,50 @@ def get_log(is_logged_in, username):
         logged = '<p>Not logged in. <a href=login>Login</a></p>'
     return logged
 
+def add_page(is_logged_in, nombre, contenido):
+    msg = ''
+    if is_logged_in:
+        page = Pages(name=nombre, page=contenido)
+        if page.name != '':
+            try:
+                page.save()
+            except IntegrityError:
+                Pages.objects.filter(name=nombre).update(page=contenido)
+                msg = 'Valor actualizado.'
+    else:
+        msg = 'Para crear o editar una pagina necesitas estar logeado.'
+    return msg
+
+@csrf_exempt
 def barra(request):
+    msg = ''
+    if request.method == 'POST':
+        msg = add_page(request.user.is_authenticated(), request.POST['name'], request.POST['page'])
+
     pages = Pages.objects.all()
     lista = 'Paginas guardadas:<ul>'
     for page in pages:
         lista += '<li><a href="' + page.name + '">' + page.name + '</a></li>'
-    lista += '</lu>'
+    lista += '</ul>'
     answer = '<h1>Sistema de gestión de contenido</h1>'
-    answer += lista
+    answer += lista + msg
+    answer += '<p>' + FORM + '</p>'
     answer += get_log(request.user.is_authenticated(), request.user.username)
     return HttpResponse(answer)
 
+@csrf_exempt
 def annotated_barra(request):
+    msg = ''
+    if request.method == 'POST':
+        msg = add_page(request.user.is_authenticated(), request.POST['name'], request.POST['page'])
+
     pages = Pages.objects.all()
     lista = 'Paginas guardadas:<ul>'
     for page in pages:
         lista += '<li><a href="' + page.name + '">' + page.name + '</a></li>'
-    lista += '</lu>'
+    lista += '</ul>'
     title = '<h1>Sistema de gestión de contenido</h1>'
-    content = lista
+    content = lista + msg + '<p>' + FORM + '</p>'
     content += get_log(request.user.is_authenticated(), request.user.username)
 
     template = get_template("annotated.html")
